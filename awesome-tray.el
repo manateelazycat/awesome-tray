df3942822fcf10d15135f690eca62faac3232d07
Andy Stewart <lazycat.manatee@gmail.com>
2022-03-17 20:06:53 +0800

    Remove cursor propertize.

--- awesome-tray.el
+++ awesome-tray.el
@@ -873,8 +873,9 @@ NAME is a string, typically a directory name."
 (defun awesome-tray-set-text (text)
   "Set the text displayed by the awesome-tray to TEXT."
   (let* ((wid (string-width text))
-         (spc (propertize " " 'cursor 1 'display
-                          `(space :align-to (- right-fringe ,wid)))))
+         (spc (propertize
+               " "
+               'display `(space :align-to (- right-fringe ,wid)))))
 
     (setq awesome-tray-text (concat spc text))
 
